cbc5fbc6af94508aa32f015efad67d37faf13b7b
moonlooker <moonlooker@hotmail.com>
2019-01-04 21:03:04 +0800

    init project

---
+++ ksBroadcastingTestClient/Broadcasting/CarViewModel.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using ksBroadcastingNetwork;
+using ksBroadcastingNetwork.Structs;
+
+namespace ksBroadcastingTestClient.Broadcasting
+{
+    public class CarViewModel : KSObservableObject
+    {
+        public int CarIndex { get; }
+        public int RaceNumber { get => Get<int>(); private set => Set(value); }
+        public int CarModelEnum { get => Get<int>(); private set => Set(value); }
+        public string TeamName { get => Get<string>(); private set => Set(value); }
+        public string TeamCarName { get => Get<string>(); private set => Set(value); }
+        public string CarModelName { get => Get<string>(); private set => Set(value); }
+        public int CupCategoryEnum { get => Get<int>(); private set => Set(value); }
+        public DriverViewModel CurrentDriver { get => Get<DriverViewModel>(); private set => Set(value); }
+
+        public IEnumerable<DriverViewModel> InactiveDrivers { get { return Drivers.Where(x => x.DriverIndex != CurrentDriver?.DriverIndex); } }
+        public ObservableCollection<DriverViewModel> Drivers { get; } = new ObservableCollection<DriverViewModel>();
+
+        public CarLocationEnum CarLocation { get => Get<CarLocationEnum>(); private set => Set(value); }
+        public int Delta { get => Get<int>(); private set => Set(value); }
+        public string DeltaString { get => Get<string>(); private set => Set(value); }
+        public Brush DeltaColor { get => Get<Brush>(); private set => Set(value); }
+        public int Gear { get => Get<int>(); private set => Set(value); }
+        public int Kmh { get => Get<int>(); private set => Set(value); }
+        public int Position { get => Get<int>(); private set => Set(value); }
+        public int CupPosition { get => Get<int>(); private set => Set(value); }
+        public int TrackPosition { get => Get<int>(); private set => Set(value); }
+        public float SplinePosition { get => Get<float>(); private set => Set(value); }
+        public float WorldX { get => Get<float>(); private set => Set(value); }
+        public float WorldY { get => Get<float>(); private set => Set(value); }
+        public float Yaw { get => Get<float>(); private set => Set(value); }
+        public int Laps { get => Get<int>(); private set => Set(value); }
+        public LapViewModel BestLap { get => Get<LapViewModel>(); private set => Set(value); }
+        public LapViewModel LastLap { get => Get<LapViewModel>(); private set => Set(value); }
+        public LapViewModel CurrentLap { get => Get<LapViewModel>(); private set => Set(value); }
+        public string LocationHint { get => Get<string>(); private set => Set(value); }
+
+        public Brush RowForeground { get => Get<Brush>(); private set => Set(value); }
+        public Brush RowBackground { get => Get<Brush>(); private set => Set(value); }
+
+
+        public CarViewModel(ushort carIndex)
+        {
+            CarIndex = carIndex;
+        }
+
+        internal void Update(CarInfo carUpdate)
+        {
+            RaceNumber = carUpdate.RaceNumber;
+            CarModelEnum = carUpdate.CarModelType;
+            TeamName = carUpdate.TeamName;
+            TeamCarName = carUpdate.TeamCarName;
+            CarModelName = carUpdate.DisplayName;
+            CupCategoryEnum = carUpdate.CupCategory;
+
+            foreach (var driverUpdate in carUpdate.Drivers)
+            {
+                var driverVM = Drivers.SingleOrDefault(x => x.DriverIndex == driverUpdate.DriverIndex);
+                if (driverVM == null)
+                {
+                    // This one is new!
+                    driverVM = new DriverViewModel(driverUpdate.DriverIndex);
+                    Drivers.Add(driverVM);
+                    NotifyUpdate(nameof(InactiveDrivers));
+                }
+
+                driverVM.Update(driverUpdate);
+            }
+        }
+
+        internal void Update(RealtimeCarUpdate carUpdate)
+        {
+            if (carUpdate.CarIndex != CarIndex)
+            {
+                System.Diagnostics.Debug.WriteLine($"Wrong {nameof(RealtimeCarUpdate)}.CarIndex {carUpdate.CarIndex} for {nameof(CarViewModel)}.CarIndex {CarIndex}");
+                return;
+            }
+
+            if (CurrentDriver?.DriverIndex != carUpdate.DriverIndex)
+            {
+                // The driver has changed!
+                CurrentDriver = Drivers.SingleOrDefault(x => x.DriverIndex == carUpdate.DriverIndex);
+                NotifyUpdate(nameof(InactiveDrivers));
+            }
+
+            CarLocation = carUpdate.CarLocation;
+            Delta = carUpdate.Delta;
+            DeltaString = $"{TimeSpan.FromMilliseconds(Delta):ss\\.f}";
+            if (Delta < -100)
+                DeltaColor = Brushes.Green;
+            else if (Delta > 100)
+                DeltaColor = Brushes.Red;
+            else
+                DeltaColor = null;
+
+            Gear = carUpdate.Gear + 1;
+            Kmh = carUpdate.Kmh;
+            Position = carUpdate.Position;
+            CupPosition = carUpdate.CupPosition;
+            TrackPosition = carUpdate.TrackPosition;
+            SplinePosition = carUpdate.SplinePosition;
+            WorldX = carUpdate.WorldPosX;
+            WorldY = carUpdate.WorldPosY;
+            Yaw = carUpdate.Yaw;
+            Laps = carUpdate.Laps;
+
+            if(BestLap == null && carUpdate.BestSessionLap != null)
+                BestLap = new LapViewModel();
+            if (carUpdate.BestSessionLap != null)
+                BestLap.Update(carUpdate.BestSessionLap);
+
+            if (LastLap == null && carUpdate.LastLap != null)
+                LastLap = new LapViewModel();
+            if (carUpdate.LastLap != null)
+                LastLap.Update(carUpdate.LastLap);
+
+            if (CurrentLap == null && carUpdate.CurrentLap != null)
+                CurrentLap = new LapViewModel();
+            if (carUpdate.CurrentLap != null)
+                CurrentLap.Update(carUpdate.CurrentLap);
+
+            // The location hint will combine stuff like pits, in/outlap
+            if (CarLocation == CarLocationEnum.PitEntry)
+                LocationHint = "InPit";
+            else if (CarLocation == CarLocationEnum.Pitlane)
+                LocationHint = "PIT";
+            else if (CarLocation == CarLocationEnum.PitExit)
+                LocationHint = "OutPit";
+            else if (CarLocation == CarLocationEnum.Track)
+                LocationHint = "OnTrack";
+            else
+                LocationHint = CurrentLap?.LapHint;
+        }
+
+        internal void SetFocused(int focusedCarIndex)
+        {
+            if (CarIndex == focusedCarIndex)
+            {
+                RowForeground = Brushes.Yellow;
+                RowBackground = Brushes.Black;
+            }
+            else
+            {
+                RowForeground = Brushes.Black;
+                RowBackground = null;
+            }
+        }
+    }
+}
---
+++ ksBroadcastingTestClient/Broadcasting/ReplayControlViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ksBroadcastingNetwork;
+
+namespace ksBroadcastingTestClient.Broadcasting
+{
+    public class ReplayControlViewModel : KSObservableObject
+    {
+        public KSRelayCommand PlayLiveReplay { get; }
+
+        public int LiveReplaySecondsBack { get => Get<int>(); private set => Set(value); }
+        public int LiveReplaySecondsPlaytime { get => Get<int>(); private set => Set(value); }
+        public int CurrentSessionTime { get => Get<int>(); private set => Set(value); }
+
+        private List<ACCUdpRemoteClient> _clients = new List<ACCUdpRemoteClient>();
+
+        public ReplayControlViewModel()
+        {
+            PlayLiveReplay = new KSRelayCommand(OnLiveReplay);
+            LiveReplaySecondsBack = 120;
+            LiveReplaySecondsPlaytime = 10;
+        }
+
+        private void OnLiveReplay(object obj)
+        {
+            try
+            {
+                var secondsBack = Convert.ToInt32(obj);
+                var requestedStartTime = CurrentSessionTime - (secondsBack * 1000);
+
+                foreach (var client in _clients)
+                {
+                    client.MessageHandler.RequestInstantReplay(requestedStartTime, secondsBack * 1000.0f);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex);
+            }
+        }
+
+        internal void RegisterNewClient(ACCUdpRemoteClient newClient)
+        {
+            newClient.MessageHandler.OnRealtimeUpdate += MessageHandler_OnRealtimeUpdate;
+            _clients.Add(newClient);
+        }
+
+        private void MessageHandler_OnRealtimeUpdate(string sender, ksBroadcastingNetwork.Structs.RealtimeUpdate update)
+        {
+            CurrentSessionTime = Convert.ToInt32(update.SessionTime.TotalMilliseconds);
+        }
+    }
+}
